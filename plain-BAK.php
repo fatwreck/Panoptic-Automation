72db04d3f41f7cccf3fb99191b0f4f3c3c0b0d0e
Dale Pidlisny <fatwreck@users.noreply.github.com>
2015-11-02 10:58:01 -0700

    First commit
    First commit of the Panoptic Automation site

---
+++ plain-BAK.php
@@ -0,0 +1,192 @@
+<!doctype html>
+<html class="no-js" lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Plain - Panoptic Automation Solutions</title>
+    <link rel="stylesheet" href="css/foundation.css" />
+    <link rel="stylesheet" href="css/styles.css" />
+    <script src="js/vendor/modernizr.js"></script>
+  </head>
+  <body>
+    <div class="off-canvas-wrap" data-offcanvas>
+      <div class="inner-wrap">
+        <nav class="tab-bar">
+          <section class="left tab-bar-section">
+            <img src="img/Panoptic-Automation-Solutions-Logo.svg" />
+          </section>
+
+          <section class="right-small">
+            <a class="right-off-canvas-toggle menu-icon" ><span></span></a>
+          </section>
+        </nav>
+
+        <aside class="left-off-canvas-menu">
+          <ul class="off-canvas-list">
+            <li><label>Foundation</label></li>
+            <li class="has-submenu"><a href="#">The Psychohistorians</a>
+                <ul class="left-submenu">
+                    <li class="back"><a href="#">Back</a></li>
+                    <li><label>Level 1</label></li>
+                    <li><a href="#">Link 1</a></li>
+                    <li class="has-submenu"><a href="#">Link 2 w/ submenu</a>
+                        <ul class="left-submenu">
+                            <li class="back"><a href="#">Back</a></li>
+                            <li><label>Level 2</label></li>
+                            <li><a href="#">...</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="#">...</a></li>
+                </ul>
+            </li>
+            <li><a href="#">The Encyclopedists</a></li>
+            <li><a href="#">The Mayors</a></li>
+            <li><a href="#">The Traders</a></li>
+            <li><a href="#">The Merchant Princes</a></li>
+          </ul>
+        </aside>
+
+        <aside class="right-off-canvas-menu">
+          <ul class="off-canvas-list">
+            <li><label>Users</label></li>
+            <li><a href="#">Hari Seldon</a></li>
+            <li class="has-submenu"><a href="#">R. Giskard Reventlov</a>
+                <ul class="right-submenu">
+                    <li class="back"><a href="#">Back</a></li>
+                    <li><label>Level 1</label></li>
+                    <li><a href="#">Link 1</a></li>
+                    <li class="has-submenu"><a href="#">Link 2 w/ submenu</a>
+                        <ul class="right-submenu">
+                            <li class="back"><a href="#">Back</a></li>
+                            <li><label>Level 2</label></li>
+                            <li><a href="#">...</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="#">...</a></li>
+                </ul>
+            </li>
+            <li><a href="#">...</a></li>
+          </ul>
+        </aside>
+
+        <section class="main-section">
+          <div class="row">
+            <div class="large-3 columns">_____________</div>
+            <div class="small-12 large-6 columns">
+              <h1>Electrical Engineering</h1>
+              <p>Panoptic Automation Solutions Engineering Department undertakes all projects with an emphasis on quality and maintaining both schedule and budget to meet our customer’s needs. Our engineering projects vary from both brownfield and greenfield control system projects, where we specify, design, and procure control panels, to projects with more extensive electrical scope. Our experienced engineers can develop single line diagrams, motor schematics, cable schedules, instrument layout drawings, cause & effect diagrams or whatever else is required to provide our clients the complete engineering solution they require for their facility. We develop electrical construction work packages complete with drawings and scope of work for use by electrical contractors for installation and wiring of process or control buildings and entire sites. We work within our clients standards to provide easily integrated engineering solutions.
+    </p>
+            </div>
+            <div class="large-3 columns"><h2>Specializing in multiple disciplines. How can we help you?</h2></div>
+          </div>
+        </section>
+
+        <a class="exit-off-canvas"></a>
+
+      </div>
+    </div>
+<!--
+    <div class="row fullWidth blue">
+      <div class="large-12 columns">
+        <div class="row">
+          <div class="off-canvas-wrap" data-offcanvas>
+            <div class="inner-wrap">
+              <nav class="tab-bar">
+                <section class="left-small">
+                  <a class="left-off-canvas-toggle menu-icon" ><span></span></a>
+                </section>
+
+                <section class="middle tab-bar-section">
+                  <img src="img/Panoptic-Automation-Solutions-Logo.svg" />
+                </section>
+
+                <section class="right-small">
+                  <a class="right-off-canvas-toggle menu-icon" ><span></span></a>
+                </section>
+              </nav>
+
+              <aside class="left-off-canvas-menu">
+                <ul class="off-canvas-list">
+                  <li><label>Foundation</label></li>
+                  <li class="has-submenu"><a href="#">The Psychohistorians</a>
+                      <ul class="left-submenu">
+                          <li class="back"><a href="#">Back</a></li>
+                          <li><label>Level 1</label></li>
+                          <li><a href="#">Link 1</a></li>
+                          <li class="has-submenu"><a href="#">Link 2 w/ submenu</a>
+                              <ul class="left-submenu">
+                                  <li class="back"><a href="#">Back</a></li>
+                                  <li><label>Level 2</label></li>
+                                  <li><a href="#">...</a></li>
+                              </ul>
+                          </li>
+                          <li><a href="#">...</a></li>
+                      </ul>
+                  </li>
+                  <li><a href="#">The Encyclopedists</a></li>
+                  <li><a href="#">The Mayors</a></li>
+                  <li><a href="#">The Traders</a></li>
+                  <li><a href="#">The Merchant Princes</a></li>
+                </ul>
+              </aside>
+
+              <aside class="right-off-canvas-menu">
+                <ul class="off-canvas-list">
+                  <li><label>Users</label></li>
+                  <li><a href="#">Hari Seldon</a></li>
+                  <li class="has-submenu"><a href="#">R. Giskard Reventlov</a>
+                      <ul class="right-submenu">
+                          <li class="back"><a href="#">Back</a></li>
+                          <li><label>Level 1</label></li>
+                          <li><a href="#">Link 1</a></li>
+                          <li class="has-submenu"><a href="#">Link 2 w/ submenu</a>
+                              <ul class="right-submenu">
+                                  <li class="back"><a href="#">Back</a></li>
+                                  <li><label>Level 2</label></li>
+                                  <li><a href="#">...</a></li>
+                              </ul>
+                          </li>
+                          <li><a href="#">...</a></li>
+                      </ul>
+                  </li>
+                  <li><a href="#">...</a></li>
+                </ul>
+              </aside>
+
+              <section class="main-section">
+                 content goes here 
+              </section>
+
+            <a class="exit-off-canvas"></a>
+
+            </div>
+          </div>
+        </div>
+        
+        
+        <div class="row">
+          <div class="small-12 large-2 columns">
+            
+          </div>
+          <div class="small-12 large-10 columns">
+            <ul class="nav">
+              <li><a href=""></a>Services</li>
+              <li><a href=""></a>Solutions</li>
+              <li><a href=""></a>News</li>
+              <li><a href=""></a>Careers</li>
+              <li><a href=""></a>About</li>
+              <li><a href=""></a>Contact</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+-->
+    
+    <script src="js/vendor/jquery.js"></script>
+    <script src="js/foundation.min.js"></script>
+    <script>
+      $(document).foundation();
+    </script>
+  </body>
+</html>
---
+++ plain.php
@@ -0,0 +1,188 @@
+<!doctype html>
+<!--[if IE 9]><html class="no-js lt-ie10" lang="en"> <![endif]-->
+<!--[if gt IE 9]><!-->
+<html class="no-js" lang="en">
+<!--<![endif]--> 
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Plain - Panoptic Automation Solutions</title>
+    <link rel="stylesheet" href="css/foundation.css" />
+    <link rel="stylesheet" href="css/styles.css" />
+    <script src="js/vendor/modernizr.js"></script>
+  </head>
+  <body>    
+    <div class="off-canvas-wrap" data-offcanvas="">
+        <div class="inner-wrap">
+          <nav class="tab-bar">
+            
+
+            <!-- LARGE SCREEN TOP BAR MENU -->
+            <section class="navigation-section">
+                <div class="row">
+                    <div class="large-12 columns">
+                        <section class="left tab-bar-section">
+                          <img src="img/Panoptic-Automation-Solutions-Logo.svg" />
+                        </section>
+                        <!-- TOP BAR INITIALIZATION -->
+                        <nav class="top-bar" data-topbar>
+                            <section class="top-bar-section show-for-large-up">
+                                <ul class="right">
+                                    <li class="active"><a href="services.html">Services</a></li>
+                                    <li><a href="">Solutions</a></li>
+                                    <li><a href="">News</a></li>
+                                    <li><a href="">Careers</a></li>
+                                    <li><a href="">About</a></li>
+                                    <li><a href="">Contact</a></li>
+<!--
+                                    <li class="has-dropdown">
+                                        <a href="blog.html">Blog</a>
+                                        <ul class="dropdown">
+                                            <li><a href="blog.html">Archive</a></li>
+                                            <li><a href="post.html">Single</a></li>
+                                        </ul>
+                                    </li>
+                                    <li class="has-dropdown">
+                                        <a href="page.html">Page</a>
+                                        <ul class="dropdown">
+                                            <li><a href="page.html">Full-wdith</a></li>
+                                            <li><a href="sidebar-left.html">Left Sidebar</a></li>
+                                            <li><a href="sidebar-right.html">Right Sidebar</a></li>
+                                        </ul>
+                                    </li>
+                                    <li><a href="portfolio.html">Portfolio</a></li>
+                                    <li class="has-dropdown">
+                                        <a href="#">Dropdown</a>
+                                        <ul class="dropdown">
+                                            <li><a href="#">First link in dropdown</a></li>
+                                            <li><a href="#">Second link in dropdown</a></li>
+                                            <li><a href="#">Third link in dropdown</a></li>
+                                        </ul>
+                                    </li>
+-->
+                                </ul>
+                            </section> <!-- END SECTION.TOP-BAR-SECTION -->
+                        </nav> <!-- END NAV.TOP-BAR -->
+                      <h1>Our Services</h1>
+                      <nav>
+                        <section class="top-bar-section show-for-large-up sub-nav">
+                            <ul class="left">
+                                <li class="active"><a href="">Electrical Engineering</a></li>
+                                <li><a href="">Instrumentation Engineering</a></li>
+                                <li><a href="">Control Systems</a></li>
+                                <li><a href="">Design &amp; Drafting</a></li>
+                                <li><a href="">Project Management</a></li>
+                            </ul>
+                        </section>
+                      </nav>
+                    </div> <!-- END 12 COLUMNS -->
+                </div> <!-- END ROW -->
+            </section> <!-- END SECTION.NAVIGATION-SECTION -->   
+            
+            <section class="right-small hide-for-large-up">
+              <a class="right-off-canvas-toggle menu-icon" ><span></span></a>
+            </section>
+          </nav>          
+
+          
+          
+          
+<!--
+            <nav class="tab-bar hide-for-large-up">
+                <section class="right-small">
+                    <a class="right-off-canvas-toggle menu-icon" ><span></span></a>
+                </section>
+            </nav>
+-->
+
+            <aside class="right-off-canvas-menu">
+              <ul class="off-canvas-list">
+                <li><label>Users</label></li>
+                <li><a href="#">Hari Seldon</a></li>
+                <li class="has-submenu"><a href="#">R. Giskard Reventlov</a>
+                    <ul class="right-submenu">
+                        <li class="back"><a href="#">Back</a></li>
+                        <li><label>Level 1</label></li>
+                        <li><a href="#">Link 1</a></li>
+                        <li class="has-submenu"><a href="#">Link 2 w/ submenu</a>
+                            <ul class="right-submenu">
+                                <li class="back"><a href="#">Back</a></li>
+                                <li><label>Level 2</label></li>
+                                <li><a href="#">...</a></li>
+                            </ul>
+                        </li>
+                        <li><a href="#">...</a></li>
+                    </ul>
+                </li>
+                <li><a href="#">...</a></li>
+              </ul>
+            </aside>
+
+<!--
+            <header>
+                <div class="row">
+                    <div class="small-12 medium-12 large-12 columns">
+                        <div class="logo left">
+                            <img src="img/Panoptic-Automation-Solutions-Logo.svg" />
+                        </div>
+                    </div>
+                </div>
+            </header>
+-->
+
+            <!-- MAIN SECTION -->
+            <section class="main-section">
+              <div class="row">
+                <div class="large-3 columns left-col show-for-large-up"></div>
+                <div class="small-12 large-6 columns">
+                  <h1>Electrical Engineering</h1>
+                  <p>Panoptic Automation Solutions Engineering Department undertakes all projects with an emphasis on quality and maintaining both schedule and budget to meet our customer’s needs. Our engineering projects vary from both brownfield and greenfield control system projects, where we specify, design, and procure control panels, to projects with more extensive electrical scope. Our experienced engineers can develop single line diagrams, motor schematics, cable schedules, instrument layout drawings, cause &amp; effect diagrams or whatever else is required to provide our clients the complete engineering solution they require for their facility. We develop electrical construction work packages complete with drawings and scope of work for use by electrical contractors for installation and wiring of process or control buildings and entire sites. We work within our clients standards to provide easily integrated engineering solutions.
+        </p>
+                </div>
+                <div class="large-3 columns right-col show-for-large-up"><h2>Specializing in multiple disciplines. How can we help you?</h2></div>
+              </div>
+            </section>
+
+            <footer>
+                <div class="row">
+                    <div class="small-12 columns">
+                        <ul>
+                            <li>Services</li>
+                            <li>Solutions</li>
+                            <li>Careers		
+                            <li>Contact
+                            <li>Awards and Testimonials</li>
+                            <li>Login</li>
+                        </ul>
+                        <img src="img/logo-panoptic-automations-dark.png" alt="Panoptic Automation" />
+                    </div>
+                </div>
+            </footer>
+
+            <!-- COPYRIGHT SECTION -->
+<!--
+            <section class="full-width copyright-section">
+                <div class="row">
+                    <div class="small-12 columns">
+                        <p>&copy; 2014</p>
+                    </div>
+                </div>
+            </section>
+-->
+                <a class="exit-off-canvas"></a>
+
+                </div>
+            </div>
+          </div>
+        </div>
+    
+    <script src="js/vendor/jquery.js"></script>
+    <script src="js/foundation.min.js"></script>
+    <script>
+      $(document).foundation();
+    </script>
+  </body>
+</html>
+
+
+
