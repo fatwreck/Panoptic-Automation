1e8f40ae5a1caf8b565d54d7ba6eb4a34d316ce2
Dale Pidlisny <fatwreck@users.noreply.github.com>
2015-11-03 10:51:07 -0700

    Edits to the plain.php styles.
    Further set up to the nav and formatting of the plain template.

--- plain.php
+++ plain.php
@@ -8,6 +8,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Plain - Panoptic Automation Solutions</title>
     <link rel="stylesheet" href="css/foundation.css" />
+    <link rel="stylesheet" href="css/foundation/_variables.css" />
     <link rel="stylesheet" href="css/styles.css" />
     <script src="js/vendor/modernizr.js"></script>
   </head>
@@ -133,13 +134,14 @@
             <!-- MAIN SECTION -->
             <section class="main-section">
               <div class="row">
-                <div class="large-3 columns left-col show-for-large-up"></div>
-                <div class="small-12 large-6 columns">
+                <div class="large-2 columns left-col show-for-large-up"></div>
+                <div class="small-12 large-8 columns white-space-right">
                   <h1>Electrical Engineering</h1>
                   <p>Panoptic Automation Solutions Engineering Department undertakes all projects with an emphasis on quality and maintaining both schedule and budget to meet our customer’s needs. Our engineering projects vary from both brownfield and greenfield control system projects, where we specify, design, and procure control panels, to projects with more extensive electrical scope. Our experienced engineers can develop single line diagrams, motor schematics, cable schedules, instrument layout drawings, cause &amp; effect diagrams or whatever else is required to provide our clients the complete engineering solution they require for their facility. We develop electrical construction work packages complete with drawings and scope of work for use by electrical contractors for installation and wiring of process or control buildings and entire sites. We work within our clients standards to provide easily integrated engineering solutions.
         </p>
                 </div>
-                <div class="large-3 columns right-col show-for-large-up"><h2>Specializing in multiple disciplines. How can we help you?</h2></div>
+                <div class="large-2 columns right-col show-for-large-up"><h2>Specializing in multiple disciplines. How can we help you?</h2>
+                  <p>We have years of experience helping companies achieve their goals and meeting budget requirements.</p></div>
               </div>
             </section>
 
@@ -147,12 +149,12 @@
                 <div class="row">
                     <div class="small-12 columns">
                         <ul>
-                            <li>Services</li>
-                            <li>Solutions</li>
-                            <li>Careers		
-                            <li>Contact
-                            <li>Awards and Testimonials</li>
-                            <li>Login</li>
+                            <li><a href="#">Services</a></li>
+                            <li><a href="#">Solutions</a></li>
+                            <li><a href="#">Careers</a></li>
+                            <li><a href="#">Contact</a></li>
+                            <li><a href="#">Awards and Testimonials</a></li>
+                            <li><a href="#">Login</a></li>
                         </ul>
                         <img src="img/logo-panoptic-automations-dark.png" alt="Panoptic Automation" />
                     </div>
